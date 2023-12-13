d79f7e450450c2b8d33ec3ae5fc3562776db2757
Lars Kellogg-Stedman <lars@redhat.com>
2023-12-13 10:41:16 -0500

    Use openshift templates to generate test resources
    This commit replaces the use of sed for templating with the `oc process`
    command [1], and makes use of labels to manage the cleanup of test artifacts.
    
    [1]: https://docs.openshift.com/container-platform/4.14/openshift_images/using-templates.html

--- scalability_test/cleanup.sh
+++ scalability_test/cleanup.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-then
-    echo "No arguments supplied. Please provide the number of notebooks to clean up."
+oc_args=()
+delete_all_tests=0
+
+while getopts aw ch; do
+    case $ch in
+        w)  oc_args+=( --wait=false )
+            ;;
+
+        a)  delete_all_tests=1
+            ;;
+
+        *)  exit 2
+            ;;
+    esac
+done
+shift $(( OPTIND - 1 ))
+
+if (( delete_all_tests )); then
+    labelarg="ope-test-run"
+elif (( $# == 0 )); then
+    echo "No arguments supplied. Please provide the name of a test run to clean up." >&2
     exit 1
+else
+    labelarg="ope-test-run=$1"
 fi
 
-for i in $(seq 0 $(( $1 - 1 ))); do
-  notebook_name="ope-scalability-$i"
-
-  # Delete notebook
-  oc delete notebook $notebook_name
-
-  # # Delete PVC
-  oc delete pvc $notebook_name
-
-  # Delete Service Account
-  oc delete serviceaccount $notebook_name
-
-done
+# Delete notebook
+oc delete notebook -l "$labelarg" "${oc_args[@]}"
+oc delete pvc -l "$labelarg" "${oc_args[@]}"
 
 echo "Resources deletion completed."
--- scalability_test/create_notebook.sh
+++ scalability_test/create_notebook.sh
@@ -1,33 +1,51 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <num_notebooks> <batch_size> <username>"
+while getopts n: ch; do
+    case $ch in
+        n)  test_run_name=$OPTARG
+            ;;
+
+        *)  exit 2
+            ;;
+    esac
+done
+shift $(( OPTIND - 1 ))
+
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $0 [-n <test-run-name>] <num_notebooks> <batch_size> <username>" >&2
     exit 1
 fi
 
 # Number of notebbooks to create
 num_notebooks=$1
+
 # Number of requests to make concurrently
 batch_size=$2
+
+# RHODS username
 username=$3
+
+if [[ -z $test_run_name ]]; then
+    test_run_name="$(mktemp -u ope-test-"$username"-"$num_notebooks"-XXXXXX)"
+fi
+
+# ensure names are lower case
+test_run_name="${test_run_name,,}"
+
+# SECONDS is a magic bash variable that will return the number of
+# elapsed seconds since it was set to 0.
 SECONDS=0
 
-create_resources() {
-    notebook_name=$1
-    # Create service account
-    sed "s/ope-placeholder/$notebook_name/g" sa_template.yaml | oc apply -f -
+echo "Starting test run $test_run_name with $num_notebooks notebooks"
 
-    # Create pvc
-    sed "s/ope-placeholder/$notebook_name/g" pvc_template.yaml | oc apply -f -
-
-    # Create notebook
-    sed -e "s/ope-placeholder/$notebook_name/g" -e "s/your_username/$username/g" notebook_template.yaml | oc apply -f -
-}
-
-for ((i=0; i<$num_notebooks; i+=batch_size)); do
-    for ((j=0; j<$batch_size && (i+j)<$num_notebooks; j++)); do
-        notebook_name="ope-scalability-$((i+j))"
-        create_resources "$notebook_name" &
+for ((i=0; i<num_notebooks; i+=batch_size)); do
+    for ((j=0; j<batch_size && (i+j)<num_notebooks; j++)); do
+        notebook_name="${test_run_name}-$((i+j))"
+        oc process -f test_resources.yaml --local \
+            -p NOTEBOOK_NAME="$notebook_name" \
+            -p TEST_RUN_NAME="$test_run_name" \
+            -p USERNAME="$username" |
+            oc create -f-
     done
     # Wait for the current batch to finish before starting the next
     wait

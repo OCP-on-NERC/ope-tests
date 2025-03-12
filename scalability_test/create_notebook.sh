f3fcaaa925baf24ffae889bc7b5656c57e946c9c
EldritchJS <jschlessman@gmail.com>
2025-03-12 11:32:10 -0400

    Added Python script for creating Tiny URL records using TinyURL API. Modified create_notebook to provide TinyURL option

--- scalability_test/create_notebook.sh
+++ scalability_test/create_notebook.sh
@@ -2,17 +2,31 @@
 
 create_resource_command=( oc create -f- )
 
-while getopts t:n:d ch; do
+csv_name="/dev/null"
+
+rate_limit = 0.5
+
+while getopts a:t:n:r:c:d ch; do
     case $ch in
+	r)  rate_limit=$OPTARG
+	    ;;
+
         t)  test_run_name=$OPTARG
             ;;
 
-	    n)  namespace=$OPTARG
+	n)  namespace=$OPTARG
             ;;
 
 	# Send manifests to stdout instead of creating resources
         d)  create_resource_command=( cat )
             ;;
+
+	c)  csv_name=$OPTARG
+            ;;
+
+	# TinyURL API token
+	a) api_token=$OPTARG
+	    ;;
 
         *)  exit 2
             ;;
@@ -21,7 +35,7 @@ done
 shift $(( OPTIND - 1 ))
 
 if [[ $# -ne 4 ]]; then
-    echo "Usage: $0 [-d] [-n <namespace>] [-t <test-run-name>] <num_notebooks> <batch_size> <username> <image_name>" >&2
+    echo "Usage: $0 [-d] [-a <TinyURL API Token>] [-r <rate_limit>] [-n <namespace>] [-t <test-run-name>] [-c <tiny_url_csv_filename>] <num_notebooks> <batch_size> <username> <image_name>" >&2
     exit 1
 fi
 
@@ -47,17 +61,23 @@ if [[ -z $namespace ]]; then
 fi
 
 # ensure names are lower case
-test_run_name="${test_run_name,,}"
+#test_run_name="${test_run_name,,}"
 
 # SECONDS is a magic bash variable that will return the number of
 # elapsed seconds since it was set to 0.
 SECONDS=0
 
-echo "Starting test run $test_run_name with $num_notebooks notebooks"
+echo "Starting test run $test_run_name with $num_notebooks notebooks" 
+
+echo "URL,Domain,Alias,Tag" > $csv_name
 
 for ((i=0; i<num_notebooks; i+=batch_size)); do
     for ((j=0; j<batch_size && (i+j)<num_notebooks; j++)); do
-        notebook_name="${test_run_name}-$((i+j))"
+	random_id=`openssl rand -hex 3`
+        notebook_name="${test_run_name}-${random_id}"
+	url="https://bmfm-${random_id}-ai4dd-06afdc.apps.shift.nerc.mghpcc.org/notebook/ai4dd-06afdc/bmfm-${random_id}/lab/workspaces/auto-s/tree/openad_notebooks/BMFM_HOL.ipynb"
+	alias="ai4dd-${random_id}"
+	echo "${url},tinyurl.com,${alias},demo" >> $csv_name
         oc process -f test_resources.yaml --local \
             -p NOTEBOOK_NAME="$notebook_name" \
             -p TEST_RUN_NAME="$test_run_name" \
@@ -65,11 +85,15 @@ for ((i=0; i<num_notebooks; i+=batch_size)); do
             -p IMAGE_NAME="$image_name" \
 	    -p NAMESPACE="$namespace" |
             "${create_resource_command[@]}"
+	if [[ -z $api_token ]]; then
+	  python add_tiny_url.txt --token $api_token --url $url --domain tinyurl.com --alias $alias --tags demo
+	  sleep $rate_limit
+	fi
     done
     # Wait for the current batch to finish before starting the next
     wait
 done
 
-echo "All notebooks are starting. The total requests time is $SECONDS seconds."
+echo "All notebooks are starting. The total time for requests is $SECONDS seconds."
 
 
